7137d2b505f1882d2a2127c3af9d71881093c64d
Mihai Dornescu <dornescu@gmail.com>
2022-06-19 17:38:55 +0300

    initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'http-fetch';
+  users :any;
+  url = 'https://api.github.com/users';
+  isLoading = false;
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit() {
+    this.getUsers()
+  }
+
+  getUsers(){
+    this.isLoading = true;
+    this.http.get(this.url).subscribe(res =>{
+      this.isLoading = false;
+      console.log(res)
+      // this.users = res;
+      setTimeout(()=>{
+        this.users = res;
+      }, 3000)
+    }, err=>{
+      console.log('error')
+      console.log(err)
+      }, ()=>{
+      console.log("gotData")
+    })
+  }
+  goToGithub(url:any){
+    window.open(url,"_blank")
+  }
 }
